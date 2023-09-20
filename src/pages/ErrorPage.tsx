a4cb42c3d3efbf935417b0deef5d732d0b273276
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-09-20 13:10:22 +0530

    App Pages

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@/shadcn_components/ui/button';
+export default function ErrorPage() {
+  const navigate = useNavigate();
+  return (
+    <>
+      <div> Go Back</div>
+      <Button variant="destructive" onClick={() => navigate(-1)}>
+        Go Back
+      </Button>
+    </>
+  );
+}
