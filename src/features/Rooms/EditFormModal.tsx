1dcbfa30296c8bfaec2a9f32ff9a12cbd9e0f4a2
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-09-25 19:05:51 +0530

    edit form modal , used zod and react hook form for validation

---
+++ src/features/Rooms/EditFormModal.tsx
@@ -0,0 +1,33 @@
+import RoomsEditForm from './EditRoomForm';
+import {
+  AlertDialog,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/shadcn_components/ui/alert-dialog';
+import { Database } from '../../../api/supabase';
+
+type RoomType = Database['public']['Tables']['rooms']['Row'];
+export default function EditFormModal({
+  modalOpen,
+  setModalOpen,
+  formDefaultValues,
+}: {
+  modalOpen: boolean;
+  setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  formDefaultValues: RoomType;
+}) {
+  return (
+    <AlertDialog open={modalOpen}>
+      <AlertDialogContent className="md:min-w-[600px]">
+        <AlertDialogHeader>
+          <AlertDialogTitle>Edit Room</AlertDialogTitle>
+          <AlertDialogDescription className="">
+            <RoomsEditForm formDefaultValues={formDefaultValues} setModalOpen={setModalOpen} />
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
