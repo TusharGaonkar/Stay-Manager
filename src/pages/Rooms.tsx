6440654466d0a3cd9095168d4bb9f91af4f018b1
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-09-25 19:10:42 +0530

    Rooms page renders components from "src/features/Rooms"

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -1,3 +1,21 @@
+import { useState } from 'react';
+import RoomsCreateForm from '../features/Rooms/CreateRoomForm';
+import RoomsTable from '../features/Rooms/RoomsTable';
+import EditFormModal from '../features/Rooms/EditFormModal';
+
 export default function Rooms() {
-  return <div>Rooms page</div>;
+  const [modalOpen, setModalOpen] = useState(false);
+  const [formDefaultValues, setFormDefaultValues] = useState({});
+  return (
+    <div className="flex flex-col items-center justify-center mt-8">
+      <h2 className="text-2xl font-semibold self-start"> Rooms</h2>
+      <RoomsTable setModalOpen={setModalOpen} setFormDefaultValues={setFormDefaultValues} />
+      <RoomsCreateForm />
+      <EditFormModal
+        modalOpen={modalOpen}
+        setModalOpen={setModalOpen}
+        formDefaultValues={formDefaultValues}
+      />
+    </div>
+  );
 }
