a489b17a0b4a99286ddfe65414e0de814e3b3dc4
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-09-27 00:53:38 +0530

    updated new components for the room page

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -2,15 +2,21 @@ import { useState } from 'react';
 import RoomsCreateForm from '../features/Rooms/CreateRoomForm';
 import RoomsTable from '../features/Rooms/RoomsTable';
 import EditFormModal from '../features/Rooms/EditFormModal';
+import FilterRoomTable from '../features/Rooms/FilterRoomTable';
+import SortRoomTable from '../features/Rooms/SortRoomTable';
 
 export default function Rooms() {
   const [modalOpen, setModalOpen] = useState(false);
   const [formDefaultValues, setFormDefaultValues] = useState({});
   return (
-    <div className="flex flex-col items-center justify-center mt-8">
+    <div className="flex flex-col items-center justify-center mt-7 p-2">
       <h2 className="text-2xl font-semibold self-start"> Rooms</h2>
+      <div className="flex flex-row justify-end items-end w-2/3 mb-3 p-3">
+        <FilterRoomTable />
+        <SortRoomTable />
+        <RoomsCreateForm />
+      </div>
       <RoomsTable setModalOpen={setModalOpen} setFormDefaultValues={setFormDefaultValues} />
-      <RoomsCreateForm />
       <EditFormModal
         modalOpen={modalOpen}
         setModalOpen={setModalOpen}
