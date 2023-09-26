5a2e95a9ce68c8d0b8e0cf4f40e49281f89e4409
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-09-27 00:52:48 +0530

    implemented client side sorting for rooms

---
+++ src/features/Rooms/SortRoomTable.tsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/shadcn_components/ui/select';
+
+export default function SortRoomTable() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [selectedSortOption, setSelectedSortOption] = useState(
+    searchParams.get('sort') || 'price-asc'
+  );
+
+  useEffect(() => {
+    const newSearchParams = new URLSearchParams(searchParams);
+    newSearchParams.set('sort', selectedSortOption);
+    setSearchParams(newSearchParams);
+  }, [selectedSortOption, searchParams, setSearchParams]);
+
+  return (
+    <Select onValueChange={(value) => setSelectedSortOption(value)} value={selectedSortOption}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Sort Rooms By" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="price-asc">Price (A-Z)</SelectItem>
+        <SelectItem value="price-desc">Price (Z-A)</SelectItem>
+        <SelectItem value="discount-asc">Discount (A-Z)</SelectItem>
+        <SelectItem value="discount-desc">Discount(Z-A)</SelectItem>
+        <SelectItem value="maxCapacity-asc">Max Capacity (A-Z)</SelectItem>
+        <SelectItem value="maxCapacity-desc">Max Capacity (Z-A)</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+}
