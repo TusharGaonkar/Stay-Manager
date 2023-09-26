7a4f02f2f2cb3564fa75296d66c1a8395eeb7080
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-09-27 00:50:39 +0530

    Sorts the booking table , calls getBookings api with sort field

---
+++ src/features/Bookings/SortBookingTable.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/shadcn_components/ui/select';
+
+function SortBookingTable({
+  setSelectedSortOption,
+}: {
+  setSelectedSortOption: React.Dispatch<React.SetStateAction<string>>;
+}) {
+  const [defaultSort, setDefaultSort] = useState('latest-bookings');
+
+  return (
+    <Select
+      onValueChange={(value) => {
+        setDefaultSort(value);
+        setSelectedSortOption(value);
+      }}
+      value={defaultSort}
+    >
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Sort Bookings By" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="latest-bookings">Latest Bookings</SelectItem>
+        <SelectItem value="oldest-bookings">Oldest Bookings</SelectItem>
+        <SelectItem value="booking-amount-asc">Amount (A-Z)</SelectItem>
+        <SelectItem value="booking-amount-desc">Amount (Z-A)</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+}
+
+export default SortBookingTable;
