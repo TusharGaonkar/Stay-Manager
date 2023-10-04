cda9047f46e9682f460410f7bd5971144272ec59
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-10-04 19:50:03 +0530

    get upcoming timeline , pulls in upcoming events ( default limit is 5)

---
+++ api/getUpcomingActivitiesApi.ts
@@ -0,0 +1,21 @@
+import supabase from './supabaseClient';
+
+export default async function getUpcomingActivities() {
+  try {
+    const currentDate = new Date();
+
+    const { data, error } = await supabase
+      .from('bookings')
+      .select('* , guests!inner(fullName,email), rooms!inner(name)')
+      .gte('endDate', currentDate.toISOString())
+      .order('endDate', { ascending: true })
+      .order('startDate', { ascending: true })
+
+      .limit(5);
+    if (error) throw error;
+
+    return data;
+  } catch (error) {
+    throw new Error(error.name);
+  }
+}
