f6065afcf3a2dccb8a83cae09b3259c83c59c3ef
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-10-04 20:02:18 +0530

    page renders Booking table component

--- src/pages/Bookings.tsx
+++ src/pages/Bookings.tsx
@@ -1,3 +1,10 @@
+import BookingTable from '../features/Bookings/BookingTable';
+
 export default function Bookings() {
-  return <div>Booking page</div>;
+  return (
+    <div className="flex flex-col items-center justify-center mt-8">
+      <h2 className="text-2xl font-semibold self-start"> All Bookings</h2>
+      <BookingTable />
+    </div>
+  );
 }
