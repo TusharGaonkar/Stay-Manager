70bc8c775b6d2c541c3d8dca2b81cc28564c8a87
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-10-04 20:03:07 +0530

    dashboard page renders filters and all the statistics

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,43 @@
+import RevenueDistribution from '../features/Dashboard/RevenueDistribution';
+import BookingDurationRadial from '../features/Dashboard/BookingDurationRadial';
+import UpcomingActivity from '../features/Dashboard/UpcomingActivity';
+import BookingsBar from '../features/Dashboard/TotalBookingsBar';
+import MostPopularRooms from '../features/Dashboard/MostPopularRooms';
+import Stats from '@/features/Dashboard/Stats';
+import FilterDashboard from '@/features/Dashboard/FilterDashboard';
+import { startOfYear } from 'date-fns';
+import { useState } from 'react';
+import MoonLoader from 'react-spinners/MoonLoader';
+import { Moon } from 'lucide-react';
+
 export default function Dashboard() {
-  return <div className="mx-auto w-full h-full font-semibold text-xl">DashBoard</div>;
+  const [startDate, setStartDate] = useState('This Year');
+  return (
+    <div className="">
+      {/* <MoonLoader size={50} color={'#fff'} loading={true} /> */}
+      <h1 className="text-3xl font-semibold m-3">Main Dashboard</h1>
+      <div className="flex justify-end mb-3">
+        <FilterDashboard setStartDate={setStartDate} />
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-5 gap-3 grid-flow-dense">
+        <Stats startDate={startDate} />
+        <div className="col-span-2">
+          <RevenueDistribution startDate={startDate} />
+        </div>
+        <div className="col-span-1">
+          <BookingDurationRadial range={startDate} />
+        </div>
+
+        <div className="col-span-2">
+          <UpcomingActivity />
+        </div>
+        <div className="col-span-2">
+          <BookingsBar startDate={startDate} />
+        </div>
+        <div className="col-span-3">
+          <MostPopularRooms startDate={startDate} />
+        </div>
+      </div>
+    </div>
+  );
 }
