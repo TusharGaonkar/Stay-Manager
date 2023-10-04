6fb8337190806e880fe4d3f7f3b6475a9c9f6c57
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2023-10-04 19:50:44 +0530

    added custom pagination for the table data

---
+++ src/features/Bookings/Pagination.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Button } from '@/shadcn_components/ui/button';
+
+const DATA_PER_PAGE = 10;
+
+export default function Pagination({
+  currentPage,
+  setCurrentPage,
+  dataSize,
+}: {
+  currentPage: number;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  dataSize: number;
+}) {
+  const maxPages = Math.ceil(dataSize / DATA_PER_PAGE);
+  const isLastPage = currentPage === maxPages;
+  const isFirstPage = currentPage === 1;
+
+  function handlePrevPage() {
+    setCurrentPage((prevPage) => (prevPage > 1 ? prevPage - 1 : prevPage));
+  }
+
+  function handleNextPage() {
+    setCurrentPage((prevPage) => (prevPage < maxPages ? prevPage + 1 : prevPage));
+  }
+
+  const startResult = (currentPage - 1) * DATA_PER_PAGE + 1;
+  const endResult = isLastPage
+    ? Math.min(currentPage * DATA_PER_PAGE, dataSize)
+    : currentPage * DATA_PER_PAGE;
+
+  return (
+    <div className="flex justify-between w-full mt-4">
+      <h1 className="self-start text-slate-300 text-sm">
+        {dataSize ? `Showing results from ${startResult} to ${endResult} of ${dataSize}` : null}
+      </h1>
+
+      <div className="space-x-3 mr-10 text-center">
+        <Button
+          variant="secondary"
+          onClick={handlePrevPage}
+          className=" text-xs rounded-3xl"
+          disabled={isFirstPage}
+        >
+          {'<- Previous Page'}
+        </Button>
+        <Button
+          variant=""
+          onClick={handleNextPage}
+          className=" text-xs rounded-3xl bg-purple-300 hover:bg-purple-400"
+          disabled={isLastPage}
+        >
+          {'Next Page ->'}
+        </Button>
+      </div>
+    </div>
+  );
+}
+
