47055a51c75e510ba5cdc206f5ecaabbd981b9dc
Tushar <gaonkar.tushar01@gmail.com>
2023-12-19 00:28:47 +0530

    fix: get country flag too in the upcoming activities

--- api/getUpcomingActivitiesApi.ts
+++ api/getUpcomingActivitiesApi.ts
@@ -6,12 +6,12 @@ export default async function getUpcomingActivities() {
 
     const { data, error } = await supabase
       .from('bookings')
-      .select('* , guests!inner(fullName,email), rooms!inner(name)')
+      .select('* , guests!inner(fullName,email,countryFlag), rooms!inner(name)')
       .gte('endDate', currentDate.toISOString())
       .order('endDate', { ascending: true })
       .order('startDate', { ascending: true })
-
       .limit(5);
+
     if (error) throw error;
 
     return data;
