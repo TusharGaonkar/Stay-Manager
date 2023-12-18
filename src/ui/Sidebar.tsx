fa2e213b03ab5d2045cab6276311a014960aec6f
Tushar <gaonkar.tushar01@gmail.com>
2023-12-19 00:49:37 +0530

    fix: highlight current active sidebar element based on the url path

--- src/ui/Sidebar.tsx
+++ src/ui/Sidebar.tsx
@@ -1,31 +1,38 @@
-/* eslint-disable import/extensions */
-import { Link } from 'react-router-dom';
-import { AiOutlineHome } from 'react-icons/ai';
+import { Link, useLocation } from 'react-router-dom';
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/shadcn_components/ui/button';
 
 type SideNavItemsType = {
   label: string;
   href: string;
-  icon: string;
+  icon: React.ComponentType;
 };
 
 export default function SideBar({ sideNavItems }: { sideNavItems: SideNavItemsType[] }) {
+  const location = useLocation();
+
   return (
-    <div className="w-1/6 min-w-[200px] h-screen flex flex-col">
-      <div className="text-3xl font-semibold p-6 text-slate-300"> Stay Manager</div>
-      {sideNavItems.map(({ label, href }) => (
-        <Link
-          to={href}
-          key={label}
-          className={cn(buttonVariants({ variant: 'ghost' }), 'justify-start', 'text-md', 'p-8')}
-        >
-          <span className="mr-2">
-            <AiOutlineHome />
-          </span>
-          {label}
-        </Link>
-      ))}
+    <div className=" h-screen flex flex-col bg-[#181818] mr-4">
+      <div className="text-3xl font-semibold p-6 text-slate-300">Stay Manager</div>
+      {sideNavItems.map(({ label, href, Icon }) => {
+        const isActive = location.pathname === href;
+
+        return (
+          <Link
+            to={href}
+            key={label}
+            className={cn(buttonVariants({ variant: 'ghost' }), 'justify-start', 'text-md', 'p-8', {
+              'bg-violet-400 bg-opacity-90': isActive,
+              'hover:bg-violet-400 hover:bg-opacity-90': isActive, // Apply highlight style if active
+            })}
+          >
+            <span className="mr-2">
+              <Icon />
+            </span>
+            {label}
+          </Link>
+        );
+      })}
     </div>
   );
 }
