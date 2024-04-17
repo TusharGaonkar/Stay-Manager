30d908911be9f9334ec550081811c51054828074
Tushar <gaonkar.tushar01@gmail.com>
2024-04-17 23:22:40 +0530

    fix: move sensitive info to .env file

--- api/supabaseClient.ts
+++ api/supabaseClient.ts
@@ -1,8 +1,10 @@
+/* eslint-disable operator-linebreak */
 import { createClient } from '@supabase/supabase-js';
 
 export const supabaseUrl = 'https://vmhotzmovgfkkcbozoey.supabase.co';
 const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtaG90em1vdmdma2tjYm96b2V5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTUyMjI3MzAsImV4cCI6MjAxMDc5ODczMH0.vCl9xdI8A2vlpeVS8vApQogGkDc_DnJpHkcIXqq3DQc';
+  (import.meta as ImportMeta & { env: Record<string, string> }).env.VITE_SUPABASE_KEY || '';
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
