274a068125905f22a00f8a4e640b4b7c9aee544f
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2024-04-25 00:56:30 +0530

    fix: change in status colors

--- src/constants/bookingTableConstants.ts
+++ src/constants/bookingTableConstants.ts
@@ -2,7 +2,7 @@ export const DATA_PER_PAGE = 10;
 
 export const statusColors: Record<string, string> = {
   unconfirmed: 'bg-gradient-to-r from-danger-200 to-danger-300 text-danger-foreground',
-  confirmed: 'bg-gradient-to-r from-warning to-warning-500',
+  confirmed: 'bg-gradient-to-r from-blue-300 to-blue-600',
   'checked out':
     'bg-[conic-gradient(at_right,_var(--tw-gradient-stops))] from-indigo-200 via-slate-600 to-indigo-200',
   'checked in': 'bg-gradient-to-r from-green-200 to-green-500',
