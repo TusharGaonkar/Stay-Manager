af6d46bac2d27ffa7ab0579d2ff1f3a041a01508
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2024-04-25 01:02:21 +0530

    feat: context to provide authenticated user data

---
+++ src/contexts/userAuthProvider.tsx
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable object-curly-newline */
+import { createContext, useEffect, useMemo, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import supabase from '../../api/supabaseClient';
+import { toast } from '@/shadcn_components/ui/use-toast';
+
+export const UserAuthContext = createContext<null | {
+  isLoading: boolean;
+  isError: boolean;
+  isSuccess: boolean;
+  userData: Record<string, unknown>;
+}>(null);
+
+export default function UserAuthProvider({ children }: { children: React.ReactNode }) {
+  const [userData, setUserData] = useState<Record<string, unknown>>({ user: null });
+
+  const { isLoading, isError, isSuccess } = useQuery({
+    queryFn: async () => {
+      const { data: { user = {} } = { data: {} } } = await supabase.auth.getUser();
+
+      return user;
+    },
+
+    queryKey: ['login'],
+    onSuccess: (user) => {
+      setUserData(user as Record<string, unknown>);
+    },
+  });
+
+  useEffect(() => {
+    if (isError) {
+      toast({
+        title: 'Error',
+        description: 'An error occured while verifying your credentials.',
+        variant: 'destructive',
+      });
+    } else if (isSuccess && !userData) {
+      toast({
+        title: 'Error',
+        description: 'You are unauthorized to access this page.',
+        variant: 'destructive',
+      });
+    }
+  }, [isError, isSuccess, userData]);
+
+  const contextValue = useMemo(
+    () => ({ isLoading, isError, isSuccess, userData }),
+    [isLoading, isError, isSuccess, userData]
+  );
+
+  return <UserAuthContext.Provider value={contextValue}>{children}</UserAuthContext.Provider>;
+}
