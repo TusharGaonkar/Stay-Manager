645bc36fbf43ac2ef88004d5faa2b256b11257d1
Tushar Gaonkar <gaonkar.tushar01@gmail.com>
2024-04-30 01:31:54 +0530

    fix: add support to all currency formats

--- src/utils/currencyFormatter.ts
+++ src/utils/currencyFormatter.ts
@@ -1,7 +1,11 @@
-export default function formatToINR(amount: number): string {
-  const formatter = new Intl.NumberFormat('en-IN', {
+const { VITE_CURRENCY_LOCALE = 'en-IN', VITE_CURRENCY = 'INR' } = (
+  import.meta as ImportMeta & { env: Record<string, string> }
+).env;
+
+export default function formatCurrency(amount: number): string {
+  const formatter = new Intl.NumberFormat(VITE_CURRENCY_LOCALE, {
     style: 'currency',
-    currency: 'INR',
+    currency: VITE_CURRENCY,
   });
   return formatter.format(amount);
 }
